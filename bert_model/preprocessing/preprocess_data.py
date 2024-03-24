e467261c4fbe9002e0fcdc874dad6d5823be15bc
adityabhalla-code <69059706+adityabhalla-code@users.noreply.github.com>
2024-03-24 22:24:50 +0530

    updated preprocessing.py

--- bert_model/preprocessing/preprocess_data.py
+++ bert_model/preprocessing/preprocess_data.py
@@ -12,7 +12,6 @@ import string
 import os
 import re
 
-print(config)
 # Function to remove HTML/XML tags from text
 def remove_html(text):
     # Create a regular expression for finding HTML tags
@@ -43,34 +42,34 @@ def preprocess_text(text):
 
 
 # # read the dataset
-# data = pd.read_csv("dataset/Reviews.csv")
-# print(f"Total number of records in the dataset--{data.shape[0]}")
-# # drop the not required variables
-# data.drop(['Id','HelpfulnessNumerator','HelpfulnessDenominator'],axis=1,inplace=True)
-#
-# # Add a sentiment column to the dataset using Score variable
-# data['Sentiment'] = data['Score'].apply(lambda x:'Positive' if x>3 else 'Negative')
-#
-# # drop the duplicate records
-# data.drop_duplicates(inplace=True)
-#
-# # change time to proper format
-# data['Time'] = pd.to_datetime(data['Time'],unit='s')
-#
-#
-# # sample the data set for 60K records
-# data_sample = data.sample(60000)
-#
-# # preprocess the data for the sample
-# data_sample['preprocessed_text'] = data_sample['Text'].apply(preprocess_text)
-#
-# # convert sentiment to numerical
-# data_sample['sentiment_score'] = data_sample['Sentiment'].apply(lambda x: 1 if 'Positive' else 0)
-#
-# # save the preprocessed data
-# file_name = 'preprocessed_'+filename
-# save_path = os.path.join(dataset_dir,file_name)
-#
-# # save the preprocessed file
-# data_sample.to_csv(save_path)
-# print(f"Preprocessing done--datset saved to {save_path}\nDataset name--{file_name}")
+data = pd.read_csv("dataset/Reviews.csv")
+print(f"Total number of records in the dataset--{data.shape[0]}")
+# drop the not required variables
+data.drop(['Id','HelpfulnessNumerator','HelpfulnessDenominator'],axis=1,inplace=True)
+
+# Add a sentiment column to the dataset using Score variable
+data['Sentiment'] = data['Score'].apply(lambda x:'Positive' if x>3 else 'Negative')
+
+# drop the duplicate records
+data.drop_duplicates(inplace=True)
+
+# change time to proper format
+data['Time'] = pd.to_datetime(data['Time'],unit='s')
+
+
+# sample the data set for 60K records
+data_sample = data.sample(60000)
+
+# preprocess the data for the sample
+data_sample['preprocessed_text'] = data_sample['Text'].apply(preprocess_text)
+
+# convert sentiment to numerical
+data_sample['sentiment_score'] = data_sample['Sentiment'].apply(lambda x: 1 if 'Positive' else 0)
+
+# save the preprocessed data
+file_name = 'preprocessed_'+filename
+save_path = os.path.join(dataset_dir,file_name)
+
+# save the preprocessed file
+data_sample.to_csv(save_path)
+print(f"Preprocessing done--datset saved to {save_path}\nDataset name--{file_name}")
